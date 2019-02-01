97784541e2c19525dc1d1123765f9eb148d4fc7b
Derek Chan <ychan3@stuy.edu>
2019-01-31 22:44:23 -0500

    added make and finished test

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
+import random
+
 ROWS = 500
 COLS = 500
-QUAL = 255
+QUAL = 250
 
 def header(file):
     file.write("P3\n#test image\n")
-    file.write("{0} {1}\n{2}".format(rows, cols, QUAL))
+    file.write("{0} {1}\n{2}\n".format(ROWS, COLS, QUAL))
+
+def body(file):
+    for row in range(ROWS):
+        for col in range(COLS):
+            y = row
+            if (y >= ROWS/2):
+                y = ROWS - row
+            x = col
+            if (x >= COLS/2):
+                x = COLS - col
+
+            r = y
+            b = x
+            g = abs(x - y)
+
+            file.write("{0} {1} {2}    ".format(r,g,b))
+        file.write("\n")
+
+f = open("test.ppm", "w")
+header(f)
+body(f)
+f.close()
